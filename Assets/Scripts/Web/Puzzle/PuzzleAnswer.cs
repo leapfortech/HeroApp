ff26c0b282bd0b1b68a6574f70cecd363084964a
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-01-21 17:18:53 -0600

    Add Status PuzzleAnswer

--- Assets/Scripts/Web/Puzzle/PuzzleAnswer.cs
+++ Assets/Scripts/Web/Puzzle/PuzzleAnswer.cs
@@ -6,14 +6,16 @@ public class PuzzleAnswer
     public long PuzzleId { get; set; }
     public String Description { get; set; }
     public int IsCorrect { get; set; }
+    public int Status { get; set; }
 
     public PuzzleAnswer() { }
 
-    public PuzzleAnswer(long id, long puzzleId, String description, int isCorrect)
+    public PuzzleAnswer(long id, long puzzleId, String description, int isCorrect, int status)
     {
         Id = id;
         PuzzleId = puzzleId;
         Description = description;
         IsCorrect = isCorrect;
+        Status = status;
     }
 }
