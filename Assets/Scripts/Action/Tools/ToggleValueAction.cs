7498ba57cd64a0a1043a228ae5ecddc779231c9a
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-02-06 15:14:48 -0600

    FIX IMAGES

---
+++ Assets/Scripts/Action/Tools/ToggleValueAction.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+using Leap.UI.Elements;
+using Leap.UI.Dialog;
+
+using Sirenix.OdinInspector;
+
+public class ToggleValueAction : MonoBehaviour
+{
+    [Title("Toggle")]
+    [SerializeField]
+    ToggleGroup[] tggs = null;
+    [Space, SerializeField]
+    String value = null;
+
+    [Title("Action")]
+    [SerializeField]
+    Button btnValidate = null;
+
+    [Title("Event")]
+    [SerializeField]
+    UnityEvent onValidated = null;
+
+
+    private void Start()
+    {
+        btnValidate?.AddAction(Validate);
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < tggs.Length; i++)
+            tggs[i].Clear();
+    }
+
+    public void Validate()
+    {
+        if (tggs == null || tggs.Length == 0)
+            return;
+
+        for (int i = 0; i < tggs.Length; i++)
+        {
+            if (String.Equals(tggs[i].Value, value, StringComparison.Ordinal))
+            {
+                onValidated?.Invoke();
+                return;
+            }
+        }
+
+        ChoiceDialog.Instance.Error("Es necesario que coloques al menos un contacto");
+    }
+}
