3807bbd952b9ccfd617b49a1c933b1ef2699812a
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-04-06 10:51:34 -0600

    New SupportPages

--- Assets/Scripts/Action/Start/LoginAction.cs
+++ Assets/Scripts/Action/Start/LoginAction.cs
@@ -26,6 +26,9 @@ public class LoginAction : MonoBehaviour
     [SerializeField]
     Text txtLoginVersion = null;
 
+    [SerializeField]
+    Text txtSupportVersion = null;
+
     [Title("Fields")]
     [SerializeField]
     InputField ifdEmail = null;
@@ -145,7 +148,7 @@ public class LoginAction : MonoBehaviour
 
     public void DisplayVersion()
     {
-        txtStartVersion.TextValue = txtLoginVersion.TextValue = "v " + Application.version + envVersion[WebManager.Instance.EnvironmentId];
+        txtStartVersion.TextValue = txtSupportVersion.TextValue = txtLoginVersion.TextValue = "v " + Application.version + envVersion[WebManager.Instance.EnvironmentId];
     }
 
     private void Start()
