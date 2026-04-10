96fe45b99284b7e88e90537da2f49ad2b26fd52b
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-04-09 21:22:59 -0600

    UpdateLocality WIP

---
+++ Assets/Scripts/Action/MenuSandwich/Locality/LocalityUpdateAction.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using UnityEngine;
+
+using Leap.UI.Elements;
+using Leap.UI.Page;
+using Leap.UI.Dialog;
+using Leap.Data.Mapper;
+
+using Sirenix.OdinInspector;
+
+public class LocalityUpdateAction : MonoBehaviour
+{
+    [Title("Params")]
+    [SerializeField]
+    bool isInterest = true;
+
+    [Title("Elements")]
+    [SerializeField]
+    ElementValue[] elementValues = null;
+
+    [Title("Data")]
+    [SerializeField]
+    DataMapper dtmLocality = null;
+
+    [Title("Action")]
+    [SerializeField]
+    Button btnUpdate = null;
+
+    [Title("Page")]
+    [SerializeField]
+    Page pagNext = null;
+
+    [Title("Message")]
+    [Space]
+    [SerializeField, TextArea(2, 4)]
+    String updatedMessage = "La información fue guardada exitosamente.";
+
+    AppUserService appUserService = null;
+
+    Locality locality = null;
+    Locality localityNew = null;
+
+    private void Awake()
+    {
+        appUserService = GetComponent<AppUserService>();
+    }
+
+    private void Start()
+    {
+        btnUpdate?.AddAction(DoUpdate);
+    }
+
+    public void Clear()
+    {
+        dtmLocality.ClearElements();
+        locality = null;
+        localityNew = null;
+    }
+
+    public void Populate()
+    {
+        //Clear();
+
+        locality = isInterest ? StateManager.Instance.InterestLocality : StateManager.Instance.CurrentLocality;
+
+        if (locality == null)
+            return;
+
+        dtmLocality.PopulateClass<Locality>(locality);
+    }
+
+    private void DoUpdate()
+    {
+        if (!ElementHelper.Validate(elementValues))
+            return;
+
+        localityNew = dtmLocality.BuildClass<Locality>();
+
+        if (!LocalityChanged())
+        {
+            ChoiceDialog.Instance.Warning("Sin cambios", "No se detectaron cambios en la información.");
+            return;
+        }
+
+        ScreenDialog.Instance.Display();
+
+        localityNew.Id = isInterest ? StateManager.Instance.InterestLocality.Id : StateManager.Instance.CurrentLocality.Id;
+        localityNew.AppUserId = StateManager.Instance.AppUser.Id;
+        localityNew.LocalityType = isInterest ? 1 : 2;
+
+        appUserService.UpdateLocality(localityNew);
+    }
+
+    public void ApplyLocality(long id)
+    {
+        if (id == -1)
+        {
+            ChoiceDialog.Instance.Error("Error", "No se pudo realizar la actualización.");
+            return;
+        }
+
+        localityNew.Id = id;
+
+        if (isInterest)
+            StateManager.Instance.InterestLocality = localityNew;
+        else
+            StateManager.Instance.CurrentLocality = localityNew;
+
+        ChoiceDialog.Instance.Info("Información actualizada", updatedMessage, () => PageManager.Instance.ChangePage(pagNext));
+    }
+
+    private bool LocalityChanged()
+    {
+        if (locality == null)
+            return localityNew.CountryId != -1
+                || localityNew.StateId != -1
+                || localityNew.CityId != -1;
+
+        return locality.CountryId != localityNew.CountryId
+            || locality.StateId != localityNew.StateId
+            || locality.CityId != localityNew.CityId;
+    }
+}
--- Assets/Scripts/Web/AppUser/Locality.cs
+++ Assets/Scripts/Web/AppUser/Locality.cs
@@ -2,21 +2,18 @@ using System;
 
 public class Locality
 {
-    public long Id { get; set; }
-    public long AppUserId { get; set; }
-    public int LocalityType { get; set; }
-    public long CountryId { get; set; }
-    public long StateId { get; set; }
-    public long CityId { get; set; }
-    public DateTime CreateDateTime { get; set; }
-    public DateTime UpdateDateTime { get; set; }
-    public int Status { get; set; }
+    public long Id { get; set; } = -1;
+    public long AppUserId { get; set; } = -1;
+    public int LocalityType { get; set; } = -1;
+    public long CountryId { get; set; } = -1;
+    public long StateId { get; set; } = -1;
+    public long CityId { get; set; } = -1;
+    public int Status { get; set; } = -1;
 
     public Locality() { }
 
     public Locality(long id, long appUserId, int localityType, long countryId,
-                    long stateId, long cityId, DateTime createDateTime, DateTime updateDateTime,
-                    int status)
+                    long stateId, long cityId, int status)
     {
         Id = id;
         AppUserId = appUserId;
@@ -24,8 +21,6 @@ public class Locality
         CountryId = countryId;
         StateId = stateId;
         CityId = cityId;
-        CreateDateTime = createDateTime;
-        UpdateDateTime = updateDateTime;
         Status = status;
     }
 }
