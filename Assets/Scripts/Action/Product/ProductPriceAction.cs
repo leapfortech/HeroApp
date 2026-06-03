810edab923f88c16b0f98725a44323a4ca0f74d7
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-06-03 09:20:59 -0600

    ProductPrice Validation

---
+++ Assets/Scripts/Action/Product/ProductPriceAction.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+using Leap.UI.Elements;
+using Leap.UI.Page;
+using Leap.UI.Dialog;
+
+using Sirenix.OdinInspector;
+
+
+public class ProductPriceAction : MonoBehaviour
+{
+    [Title("Elements")]
+    [SerializeField]
+    ElementValue[] elementValues = null;
+
+    [Space, Title("Details")]
+    [SerializeField]
+    InputField ifdPrice = null;
+    [SerializeField]
+    InputField ifdDiscountPrice = null;
+
+    [Title("Action")]
+    [SerializeField]
+    Button btnValidate = null;
+
+    [Title("Page")]
+    [SerializeField]
+    Page pagNext = null;
+
+    private void Start()
+    {
+        btnValidate?.AddAction(Validate);
+    }
+
+    public void Validate()
+    {
+        double.TryParse(ifdPrice.Text, out double price);
+        double.TryParse(ifdDiscountPrice.Text, out double discountPrice);
+
+        if (discountPrice > 0 && discountPrice > price)
+        {
+            ChoiceDialog.Instance.Error("Precio de descuento", "El precio de descuento es mayor al precio regular.");
+            return;
+        }
+
+        if (!ElementHelper.Validate(elementValues))
+            return;
+
+        PageManager.Instance.ChangePage(pagNext);
+    }
+}
