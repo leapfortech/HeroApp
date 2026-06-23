574077e5fa63375da6522a9c3297d8febb0c56ad
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-06-22 21:17:23 -0600

    Referred Copy & Share Link
    UPD ReferringCode AppUser

---
+++ Assets/Scripts/Action/MenuSandwich/Referrals/ReferredAction.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using UnityEngine;
+
+using Leap.Graphics.Tools;
+using Leap.UI.Elements;
+using Leap.UI.Dialog;
+using UnityEngine.Networking;
+
+public class ReferredAction : MonoBehaviour
+{
+    [SerializeField]
+    Text txtCode = null;
+
+    [SerializeField]
+    Button btnCopyLink = null;
+    [SerializeField]
+    Button btnShareLink = null;
+
+    private void Start()
+    {
+        btnCopyLink?.AddAction(CopyLink);
+        btnShareLink?.AddAction(ShareLink);
+    }
+
+    public void DisplayCode()
+    {
+        txtCode.TextValue = StateManager.Instance.AppUser.ReferringCode;
+    }
+
+    private void CopyLink()
+    {
+        String referringMessage = AppManager.Instance.GetParamValue("ReferringMessage");
+        GUIUtility.systemCopyBuffer = referringMessage;
+    }
+    
+    private void ShareLink()
+    {
+        String referringMessage = AppManager.Instance.GetParamValue("ReferringMessage");
+
+        referringMessage = referringMessage.Replace("ReferringCode", StateManager.Instance.AppUser.ReferringCode);
+
+        String url = "https://wa.me/?text=" + UnityWebRequest.EscapeURL(referringMessage);
+
+        Application.OpenURL(url);
+    }
+}
--- Assets/Scripts/Web/AppUser/AppUser.cs
+++ Assets/Scripts/Web/AppUser/AppUser.cs
@@ -7,7 +7,10 @@ public class AppUser
     [ShowInInspector]
     public long Id { get; set; }
     public long WebSysUserId { get; set; }
+    [ShowInInspector]
     public String Alias { get; set; }
+    [ShowInInspector]
+    public String ReferringCode { get; set; }
     public String CSToken { get; set; }
     public long Options { get; set; } = 11;
     public long ReferrerAppUserId { get; set; }
@@ -18,12 +21,13 @@ public class AppUser
     {
     }
 
-    public AppUser(long id, long webSysUserId, String alias, String csToken, long options, long referrerAppUserId,
+    public AppUser(long id, long webSysUserId, String alias, String referringCode, String csToken, long options, long referrerAppUserId,
                    int appUserStatusId)
     {
         Id = id;
         WebSysUserId = webSysUserId;
         Alias = alias;
+        ReferringCode = referringCode;
         CSToken = csToken;
         Options = options;
         ReferrerAppUserId = referrerAppUserId;
