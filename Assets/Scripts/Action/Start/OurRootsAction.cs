5347d9835a9b6d85f057d5fc2f90abf13cada416
leapfortech <46363814+miralbes@users.noreply.github.com>
2026-08-26 10:57:26 -0600

    Display Treatment PagOurRoots

---
+++ Assets/Scripts/Action/Start/OurRootsAction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+
+using Leap.UI.Elements;
+
+using Sirenix.OdinInspector;
+
+public class OurRootsAction : MonoBehaviour
+{
+    [Title("Actions")]
+    [SerializeField]
+    Button btnTreatment = null;
+
+    public void RefreshOurRoots()
+    {
+        String treatmentsEnabled =  AppManager.Instance.GetParamValue("TreatmentsEnabled");
+        btnTreatment.gameObject.SetActive(treatmentsEnabled == "1");
+    }
+}
